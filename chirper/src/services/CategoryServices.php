83fb41b263e720f815415afc6d982a0200e51422
psfrancieli <francielisilva2011@gmail.com>
2026-07-08 11:22:55 -0300

    criando métodos da classe #26

--- chirper/src/services/CategoryServices.php
+++ chirper/src/services/CategoryServices.php
@@ -3,9 +3,25 @@ require_once __DIR__ . '/../repositories/CategoryRepository.php';
 
 class CategoryServices {
 
-    public function criarCategoria(string $nome) {
+    public function EncontrarCategoriaPorId(Category $categoria, int $id): int {
         $categoria = new CategoryRepository();
-        $nome = ucfirst(trim($nome));
+        $idCategoria = $categoria->EncontrarCategoriaPorId($id);
+
+        if ($idCategoria <= 0) {
+            throw new Exception("ID inválido");
+        }
+        return $id;
+    }
+
+    public function EncontrarTodasCategorias(int $id) {}
+
+    public function EncontrarCategoriaPorNome(string $nome) {
+
+    }
+
+    public function criarCategoria(string $nome): string {
+        $categoria = new CategoryRepository();
+        $nome = strtolower(trim($nome));
 
         if ($nome == '') {
             throw new InvalidArgumentException('Insira o nome da categoria para continuar.');
@@ -18,7 +34,12 @@ class CategoryServices {
         if ($categoria->EncontrarCategoriaPorNome($nome) !== null) {
             throw new InvalidArgumentException('Já existe uma categoria com esse nome.');
         }
-        
+        return $nome;
     }
+
+    public function atualizarCategoria() {}
+
+    // adicionar deletarCategoria() no CategoryRepository
 }
+
 ?>
