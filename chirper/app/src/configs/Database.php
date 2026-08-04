7ea2db406846dff19ea387227e492f6013bff3c7
Riq <ruylivinginadream@duck.com>
2026-08-04 10:57:44 -0300

    Atualizando branch

--- chirper/app/src/configs/Database.php
+++ chirper/app/src/configs/Database.php
@@ -1,15 +1,15 @@
 <?php
- 
+
 class Database
 {
     private static ?PDO $pdo = null;
- 
+
     public static function getConnection(): PDO
     {
         if (self::$pdo !== null) {
             return self::$pdo;
         }
- 
+
         try {
 
             $env = parse_ini_file(__DIR__ . '/../../../../.env');
@@ -17,7 +17,7 @@ class Database
             $host = "ep-green-night-acel3qx9-pooler.sa-east-1.aws.neon.tech";
             $dbname = "neondb";
             $port = 5432;
- 
+
             $user = $env['PGUSER'];
             $password = $env['PGPASSWORD'];
  
@@ -28,26 +28,26 @@ class Database
                 $user,
                 $password
             );
- 
+
             self::$pdo->setAttribute(
                 PDO::ATTR_ERRMODE,
                 PDO::ERRMODE_EXCEPTION
             );
- 
+
             self::$pdo->setAttribute(
                 PDO::ATTR_DEFAULT_FETCH_MODE,
                 PDO::FETCH_ASSOC
             );
- 
+
             return self::$pdo;
- 
+
         } catch (PDOException $e) {
- 
+
             throw new Exception(
                 "Erro ao conectar com banco: " . $e->getMessage()
             );
         }
     }
- 
+
 }
- 
+?>
--- chirper/app/src/controllers/TicketController.php
+++ chirper/app/src/controllers/TicketController.php
@@ -15,7 +15,29 @@ class TicketController {
         $this->repository = new TicketRepository();
     }
 
-    public function listar(): array {
+    private function validarDadosTicket(array $dados): void {
+        if (empty($dados['titulo'])) {
+            throw new InvalidArgumentException("O titulo é obrigatório.");
+        }
+
+        if (empty($dados['descricao'])) {
+            throw new InvalidArgumentException("A descrição é obrigatória.");
+        }
+
+        if (empty($dados['id_usuario'])) {
+            throw new InvalidArgumentException("O usuario responsável pela abertura é obrigatório");
+        }
+
+        if (isset($dados['prioridade']) && !in_array($dados['prioridade'], ['baixa', 'media', 'alta'], true)) {
+            throw new InvalidArgumentException("Prioridade inválida. Use: baixa, media ou alta.");
+        }
+
+        if (isset($dados['status']) && !in_array($dados['status'], ['pendente', 'concluido', 'cancelado'], true)) {
+            throw new InvalidArgumentException("Status invalido.");
+        }
+    }
+
+    public function listarTicket(): array {
         try {
             $dados = $this->repository->listarTodos();
             
@@ -26,8 +48,6 @@ class TicketController {
 
         } catch (RuntimeException $e) {
             
-        http_response_code(500);
-
             return [
                 'status' => 'error',
                 'message' => 'Erro ao listar tickets: ' . $e->getMessage()
@@ -35,12 +55,10 @@ class TicketController {
         }
     }
 
-    public function exibir(int $id): array {
+    public function exibirTicket(int $id): array {
         try {
             $ticket = $this->repository->EncontrarTicketPorId($id);
             
-            http_response_code(200);
-
             return [
                 'status' => 'success',
                 'data' => $ticket->getAll()
@@ -48,17 +66,18 @@ class TicketController {
             
         } catch (RuntimeException $e) {
 
-            http_response_code(404);
-
             return [
                 'status' => 'error',
-                'message' => 'Ticket não encontrado.'
+                'message' => 'Erro ao buscar Ticket.' 
             ];
         }
     }
 
     public function criar(array $dadosRequisicao): array {
         try {
+
+            $this->validarDadosTicket($dadosRequisicao);
+
             $novoTicket = new Ticket(
                 id: null,
                 uuid: null, 
@@ -66,7 +85,7 @@ class TicketController {
                 descricao: $dadosRequisicao['descricao'] ?? '',
                 prioridade: $dadosRequisicao['prioridade'] ?? null,
                 patrimonio: $dadosRequisicao['patrimonio'] ?? '',
-                status: $dadosRequisicao['status'] ?? 'Aberto',
+                status: $dadosRequisicao['status'] ?? 'pendente',
                 id_categoria: $dadosRequisicao['id_categoria'] ?? null,
                 id_usuario: $dadosRequisicao['id_usuario'] ?? 0,
                 id_responsavel: $dadosRequisicao['id_responsavel'] ?? null,
@@ -78,35 +97,38 @@ class TicketController {
 
             return [
                 'status' => 'success',
-                'message' => 'Ticket criado com sucesso!'
+                'message' => 'Ticket criado com sucesso'
             ];
 
         } catch (InvalidArgumentException $e) {
             return ['status' => 'error', 'message' => $e->getMessage()];
         } catch (RuntimeException $e) {
-            return ['status' => 'error', 'message' => 'Erro no banco: ' . $e->getMessage()];
+            return ['status' => 'error', 'message' => error_log($e->getMessage())];
         }
     }
 
     public function atualizarPrioridade(int $id, array $dadosRequisicao): array {
-        try {
-            if (empty($dadosRequisicao['prioridade'])) {
-                throw new InvalidArgumentException("A prioridade é obrigatória.");
-            }
+    try {
+        if (empty($dadosRequisicao['prioridade'])) {
+            throw new InvalidArgumentException("A prioridade é obrigatória.");
+        }
 
-            $this->repository->atualizarPrioridadeTicket($id, $dadosRequisicao['prioridade']);
+        $this->repository->atualizarPrioridadeTicket($id, $dadosRequisicao['prioridade']);
 
-            return [
-                'status' => 'success',
-                'message' => "Prioridade do ticket {$id} atualizada com sucesso."
-            ];
+        return [
+            'status' => 'success',
+            'message' => "Prioridade do ticket {$id} atualizada com sucesso."
+        ];
 
-        } catch (Exception $e) {
+        } catch (InvalidArgumentException $e) {
             return ['status' => 'error', 'message' => $e->getMessage()];
+
+        } catch (RuntimeException $e) {
+            return ['status' => 'error', 'message' => error_log($e->getMessage())];
         }
     }
     
-    public function encerrar(int $id, array $dadosRequisicao): array {
+    public function encerrarTicket(int $id, array $dadosRequisicao): array {
         try {
             $status = $dadosRequisicao['status'] ?? 'concluido';
             
@@ -118,9 +140,60 @@ class TicketController {
             ];
 
         } catch (RuntimeException $e) {
-            return ['status' => 'error', 'message' => $e->getMessage()];
+            return ['status' => 'error', 'message' => 'Não foi possivel encerrar' . $e->getMessage()];
         }
     }
 }
+
+//Testes
+$controller = new TicketController();
+
+// //Listagem
+// $respostaListar = $controller->listarTicket();
+// print_r($respostaListar);
+// echo "<hr>";
+
+// // Criar
+// $dadosNovoTicket = [
+//     'id' => '1111',
+//     'titulo' => 'Teclado parou de funcionar',
+//     'descricao' => 'As teclas de espaço e enter não respondem.',
+//     'prioridade' => 'media',
+//     'patrimonio' => 'PAT-102030',
+//     'status' => 'pendente',
+//     'id_categoria' => 2,
+//     'id_usuario' => 1,
+//     'id_responsavel' => null
+// ];
+// $respostaCriar = $controller->criar($dadosNovoTicket);
+// print_r($respostaCriar);
+// echo "<hr>";
+
+// //Exibir pelo id expecifico 
+// $idExibir = 1111; 
+// $respostaExibir = $controller->exibirTicket($idExibir);
+// print_r($respostaExibir);
+// echo "<hr>";
+
+
+// //Atualizar prioridade
+// $idAtualizar = 317; 
+// $dadosPrioridade = [
+//     'prioridade' => 'alta'
+// ];
+// $respostaAtualizar = $controller->atualizarPrioridade($idAtualizar, $dadosPrioridade);
+// print_r($respostaAtualizar);
+// echo "<hr>";
+
+// //Encerrar ticket
+// $idEncerrar = 317; 
+// $dadosEncerrar = [
+//     'status' => 'concluido'
+// ];
+// $respostaEncerrar = $controller->encerrarTicket($idEncerrar, $dadosEncerrar);
+// print_r($respostaEncerrar);
+// echo "<hr>";
+
+// echo "</pre>";
 
 ?>
