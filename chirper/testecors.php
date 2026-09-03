aa7ee222d2b764c8a090e1eacfbe2f0035c30143
psfrancieli <francielisilva2011@gmail.com>
2026-09-03 16:29:47 -0300

    teste cors

---
+++ chirper/testecors.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+echo json_encode([
+    'success' => true,
+    'message' => 'CORS funcionando',
+]);
